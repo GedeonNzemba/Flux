3876b6fce5e88c3f9748182b85764087b494fc5f
GedeonNzemba <nzemba48@gmail.com>
2021-06-25 08:23:57 +0200

    updated queries

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -3,8 +3,13 @@ import Image from "next/image"
 import Link from "next/link"
 import styles from '../../styles/Global.module.css'
 import Head from "next/head"
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+// import fetcher from "../../lib/wordpress/fetcher"
+// import { ALL_POSTS } from "../../lib/wordpress/api"
 
-function Header() {
+function Header({ posts }) {
+
+    console.log(posts);
     return (
         <Fragment>
             <header className={styles.header}>
@@ -35,6 +40,18 @@ function Header() {
                                     <Image src="/image/assets/Group31.png" alt="graphic image" width={46} height={46} />
                                 </div>
                             </div>
+                            {/* {posts.map((post) => {
+                                return (
+                                    <div className={styles.postedC} key={post.slug}>
+                                        <h3>{post.title}</h3>
+                                        <div dangerouslySetInnerHTML={{ __html: blogPost.excerpt }} />
+                                        <p>{post.date}</p>
+                                        <Link href={`/post/${post.slug}`}>
+                                            <a>Read more</a>
+                                        </Link>
+                                    </div>
+                                )
+                            })} */}
                             <div className={styles.__contentTitle}>
                                 <h5>a remote island sanctuary</h5>
                             </div>
@@ -56,4 +73,35 @@ function Header() {
     )
 }
 
-export default Header
+export default Header;
+
+export async function getStaticProps() {
+    const client = new ApolloClient({
+        uri: "https://playground.fluxfullcircle.com/wp/graphql",
+        cache: new InMemoryCache()
+    });
+
+    const { data } = await client.query({
+        query: gql`
+                    {
+        posts(first: 20, where: {orderby: {field: DATE, order: DESC}}) {
+            nodes {
+            id
+            date
+            slug
+            title
+            excerpt
+            }
+        }
+        }
+        `
+    })
+
+    return {
+        props: {
+            posts: data.posts.nodes
+        }
+    }
+
+
+}
